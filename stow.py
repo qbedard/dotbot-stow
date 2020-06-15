a5c9cb2a067cd8cad775c09d52fd20badba4648f
Tim Bedard <tpbedard@gmail.com>
2020-06-15 01:07:47 -0500

    basic functionality with manual testing

---
+++ stow.py
@@ -0,0 +1,58 @@
+import subprocess
+
+from dotbot import Plugin
+
+
+class Stow(Plugin):
+    def can_handle(self, directive):
+        pass
+
+    def handle(self, directive, data):
+        if directive != self._directive:
+            raise ValueError("Stow cannot handle directive %s" % directive)
+        return self._process_packages(data)
+
+    def _process_packages(self, packages):
+        success = True
+        defaults = self._context.defaults().get("stow", {})
+        if isinstance(packages, str):
+            options = defaults.copy()
+            options["package"] = packages
+            self._stow(**options)
+        elif isinstance(packages, list):
+            for package in packages:
+                options = defaults.copy()
+                options["package"] = package
+                self._stow(**options)
+        elif isinstance(packages, dict):
+            for package, value in packages.items():
+                options = defaults.copy()
+                if isinstance(value, dict):
+                    options["package"] = package
+                    options.update(value)
+                else:
+                    options.update({
+                        "package": package,
+                        "target": value,
+                    })
+                self._stow(**options)
+        return success
+
+    def _stow(self, package=".", target=None, restow=True, adopt=False, **kwargs):
+        options = [
+            "--dir={}".format(self._context.base_directory()),
+            "--target={}".format(target) if target else None,
+            "--restow" if restow else "--stow",
+            "--adopt" if adopt else None,
+        ]
+
+        for ptn_option in (o for o in ("ignore", "defer", "override") if o in kwargs):
+            ptns = kwargs.get(ptn_option)
+            if not isinstance(ptns, list):
+                ptns = [ptns]
+            for ptn in ptns:
+                options.append("--{}={}".format(ptn_option, ptn))
+
+        cmd = ["stow"] + [o for o in options if o] + [package]
+        # subprocess.check_output(cmd)
+        print(cmd)
